06d8582a74156e8288ad9e83d899a1c15855b402
[VitorVieira20] <[2078320@student.uma.pt]>
2023-08-24 18:22:10 +0100

    feat [TrainerHandler, TrainerMenuHandler] : Now is it possible o see all the trainers, add new trainers, associate trainers to teams and delete trainers

--- src/main/java/TrainerHandler.java
+++ src/main/java/TrainerHandler.java
@@ -1,7 +1,308 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class TrainerHandler {
     private String trainerName;
     private String trainerTeamName;
     private String trainerTeamGender;
 
+    public TrainerHandler(String trainerName, String trainerTeamName, String trainerTeamGender) {
+        this.trainerName = trainerName;
+        this.trainerTeamName = trainerTeamName;
+        this.trainerTeamGender = trainerTeamGender;
+    }
 
+    public void showAllTrainers() {
+        String trainersSql = "SELECT trainers.id as id, trainers.name as name, GROUP_CONCAT(CONCAT(teams.team_name, ' (', teams.team_gender, ')')) as teams_info, COUNT(teams.id) as team_count " +
+                "FROM trainers " +
+                "INNER JOIN trainers_has_teams ON trainers.id = trainers_has_teams.trainers_id " +
+                "INNER JOIN teams ON trainers_has_teams.teams_id = teams.id " +
+                "GROUP BY trainers.id, trainers.name";
+
+        String trainersNoTeamSql = "SELECT trainers.id as id, trainers.name as name " +
+                "FROM trainers " +
+                "LEFT JOIN trainers_has_teams ON trainers.id = trainers_has_teams.trainers_id " +
+                "WHERE trainers_has_teams.trainers_id IS NULL;";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement getTrainers = connection.prepareStatement(trainersSql);
+            ResultSet trainersSet = getTrainers.executeQuery();
+
+            if (!trainersSet.isBeforeFirst()) {
+                System.out.println("Ainda não existem treinador associados a equipas");
+            } else {
+                System.out.println("Treinadores com equipas associadas");
+                while (trainersSet.next()) {
+                    trainerName = trainersSet.getString("name");
+                    int trainerId = trainersSet.getInt("id");
+                    String trainerTeamsInfo = trainersSet.getString("teams_info");
+
+                    System.out.println("----------------------");
+                    System.out.println("ID: " + trainerId);
+                    System.out.println("Nome: " + trainerName);
+                    int teamCount = trainersSet.getInt("team_count");
+
+                    if (teamCount == 1) {
+                        System.out.println("Equipa: " + trainerTeamsInfo);
+                    } else {
+                        System.out.println("Equipas: " + trainerTeamsInfo);
+                    }
+                }
+            }
+
+            PreparedStatement getTrainersNoTeam = connection.prepareStatement(trainersNoTeamSql);
+            ResultSet trainersNoTeam = getTrainersNoTeam.executeQuery();
+
+            if (trainersNoTeam.isBeforeFirst()) {
+                System.out.println("");
+                System.out.println("Treinadores sem equipas associadas");
+                while (trainersNoTeam.next()) {
+                    trainerName = trainersNoTeam.getString("name");
+                    int trainerId = trainersNoTeam.getInt("id");
+                    System.out.println("----------------------");
+                    System.out.println("ID: " + trainerId);
+                    System.out.println("Nome: " + trainerName);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertTrainer() {
+        String insertSql = "INSERT INTO trainers (name) VALUES (?)";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(insertSql);
+            ps.setString(1, trainerName);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getLastTrainerInserted() {
+        String getTrainer = "SELECT id FROM trainers ORDER BY id DESC LIMIT 1;";
+        int trainerId = -1;
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(getTrainer);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                trainerId = resultSet.getInt("id");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return trainerId;
+    }
+
+    public int getTeamId() {
+        String getTeam = "SELECT * FROM teams WHERE team_name = ? AND team_gender = ?";
+        int teamId = -1;
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(getTeam);
+            ps.setString(1, trainerTeamName);
+            ps.setString(2, trainerTeamGender);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                teamId = resultSet.getInt("id");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return teamId;
+    }
+
+    public void addTrainerToTeam(int trainerId, int teamId){
+        String addTrainerToTeam = "INSERT INTO trainers_has_teams (trainers_id, teams_id) VALUES (?, ?)";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(addTrainerToTeam);
+            ps.setInt(1, trainerId);
+            ps.setInt(2, teamId);
+            ps.executeUpdate();
+            ps.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void showTrainersList() {
+        String sql = "SELECT * FROM trainers";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("Não existem treinadores");
+            } else {
+                while (resultSet.next()) {
+                    int trainerId = resultSet.getInt("id");
+                    trainerName = resultSet.getString("name");
+                    System.out.println("-------------------");
+                    System.out.println("ID: " + trainerId);
+                    System.out.println("Nome: " + trainerName);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isValidId(int trainerId) {
+        String sql = "SELECT * FROM trainers WHERE id = ?";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, trainerId);
+            ResultSet resultSet = ps.executeQuery();
+
+            if (!resultSet.isBeforeFirst()) {
+                System.out.println("Este treinador não existe");
+                return false;
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean seeAlreadyAssociate(int trainerId, int teamId) {
+        String sql = "SELECT * FROM trainers_has_teams WHERE trainers_id = ? AND teams_id = ?";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, trainerId);
+            ps.setInt(2, teamId);
+            ResultSet resultSet = ps.executeQuery();
+
+            if (!resultSet.isBeforeFirst()) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertIntoTeam(int trainerId, int teamId) {
+        String sql = "INSERT INTO trainers_has_teams (trainers_id, teams_id) VALUES (?, ?)";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, trainerId);
+            ps.setInt(2, teamId);
+            ps.executeUpdate();
+            ps.close();
+
+            System.out.println("Treinador associado a equipa com sucesso!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void showTrainerById(int trainerId) {
+        Scanner scanner = new Scanner(System.in);
+        String getTrainerSql = "SELECT * FROM trainers WHERE id = ?";
+
+        try {
+            Connection connection = Conn.getConn();
+            PreparedStatement ps = connection.prepareStatement(getTrainerSql);
+            ps.setInt(1, trainerId);
+            ResultSet getTrainerResult = ps.executeQuery();
+
+            if (!getTrainerResult.isBeforeFirst()) {
+                System.out.println("Treinador não encontrado.");
+                return;
+            }
+
+            while (getTrainerResult.next()) {
+                trainerName = getTrainerResult.getString("name");
+                System.out.println("----------------------");
+                System.out.println("ID: " + trainerId);
+                System.out.println("Nome: " + trainerName);
+            }
+
+            System.out.println("Deseja mesmo apagar este treinador?");
+            System.out.println("1- Sim");
+            System.out.println("2- Não");
+
+            while (true) {
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        String trainerAssociate = "SELECT * FROM trainers_has_teams WHERE trainers_id = ?";
+
+                        try {
+                            PreparedStatement trainer = connection.prepareStatement(trainerAssociate);
+                            trainer.setInt(1, trainerId);
+                            ResultSet trainerResult = trainer.executeQuery();
+
+                            if (!trainerResult.isBeforeFirst()) {
+                                String deleteTrainer = "DELETE FROM trainers WHERE trainers.id = ?";
+                                try {
+                                    PreparedStatement delete = connection.prepareStatement(deleteTrainer);
+                                    delete.setInt(1, trainerId);
+                                    delete.executeUpdate();
+                                    delete.close();
+                                    System.out.println("Treinador apagado com sucesso!");
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            } else {
+                                String deleteFisrtTable = "DELETE FROM trainers_has_teams WHERE trainers_has_teams.trainers_id = ?";
+                                String deleteSecondTable = "DELETE FROM trainers WHERE trainers.id = ?";
+
+                                try {
+                                    PreparedStatement firstTable = connection.prepareStatement(deleteFisrtTable);
+                                    firstTable.setInt(1, trainerId);
+                                    ;
+                                    firstTable.executeUpdate();
+                                    firstTable.close();
+
+                                    PreparedStatement secondTable = connection.prepareStatement(deleteSecondTable);
+                                    secondTable.setInt(1, trainerId);
+                                    secondTable.executeUpdate();
+                                    secondTable.close();
+                                    System.out.println("Treinador apagado com sucesso!");
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return;
+                    case 2:
+                        return;
+                    default:
+                        System.out.println("Opção incorreta: " + choice);
+                        System.out.println("Por favor, escolha uma opção válida.");
+                }
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
--- src/main/java/TrainerMenuHandler.java
+++ src/main/java/TrainerMenuHandler.java
@@ -4,14 +4,89 @@ public class TrainerMenuHandler {
     private String trainerName;
     private String trainerTeamName;
     private String trainerTeamGender;
+    public String showTeamOptions() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Escolha o escalão do atleta:");
+            System.out.println();
+            System.out.println(" 1-  Babybasket");
+            System.out.println(" 2-  Mini-8");
+            System.out.println(" 3-  Mini-10");
+            System.out.println(" 4-  Mini-12");
+            System.out.println(" 5-  Sub-14");
+            System.out.println(" 6-  Sub-16");
+            System.out.println(" 7-  Sub-18");
+            System.out.println(" 8-  Sub-19");
+            System.out.println(" 9-  Seniores");
+
+            while (true) {
+                int teamChoice = scanner.nextInt();
+                switch (teamChoice) {
+                    case 1:
+                        trainerTeamName = "babybasket";
+                        return trainerTeamName;
+                    case 2:
+                        trainerTeamName = "mini-8";
+                        return trainerTeamName;
+                    case 3:
+                        trainerTeamName = "mini-10";
+                        return trainerTeamName;
+                    case 4:
+                        trainerTeamName = "mini-1";
+                        return trainerTeamName;
+                    case 5:
+                        trainerTeamName = "sub-14";
+                        return trainerTeamName;
+                    case 6:
+                        trainerTeamName = "sub-16";
+                        return trainerTeamName;
+                    case 7:
+                        trainerTeamName = "sub-18";
+                        return trainerTeamName;
+                    case 8:
+                        trainerTeamName = "sub-19";
+                        return trainerTeamName;
+                    case 9:
+                        trainerTeamName = "seniores";
+                        return trainerTeamName;
+                    default:
+                        System.out.println("Opção incorreta: " + teamChoice);
+                        System.out.println("Por favor, escolha uma opção válida.");
+                }
+            }
+        }
+    }
+    public String showGenderOptions() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A equipa é de masculinos ou femininos?");
+        System.out.println();
+        System.out.println(" 1-  Masculinos");
+        System.out.println(" 2-  Femininos");
+
+        while (true) {
+            int genderChoice = scanner.nextInt();
+            switch (genderChoice) {
+                case 1:
+                    trainerTeamGender = "male";
+                    return trainerTeamGender;
+                case 2:
+                    trainerTeamGender = "female";
+                    return trainerTeamGender;
+                default:
+                    System.out.println("Opção incorreta: " + genderChoice);
+                    System.out.println("Por favor, escolha uma opção válida.");
+            }
+        }
+    }
     public void showTrainersMenu() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("--------------Menu Treinadores-------------");
         System.out.println("|                                         |");
         System.out.println("| 1- Ver Treinadores                      |");
-        System.out.println("| 2- Novo Treinadore                      |");
-        System.out.println("| 3- Associar Treinadore                  |");
-        System.out.println("| 4- Apagar Treinadore                    |");
+        System.out.println("| 2- Novo Treinador                       |");
+        System.out.println("| 3- Associar Treinador                   |");
+        System.out.println("| 4- Apagar Treinador                     |");
         System.out.println("| 5- Voltar                               |");
         System.out.println("|_________________________________________|");
 
@@ -19,10 +94,10 @@ public class TrainerMenuHandler {
             int athletesChoice = scanner.nextInt();
             switch (athletesChoice) {
                 case 1:
-                    showTrainersFilterTeam();
+                    showTrainers();
                     return;
                 case 2:
-                    addTrainer(false);
+                    addTrainerMenu();
                     showTrainersMenu();
                     return;
                 case 3:
@@ -41,16 +116,136 @@ public class TrainerMenuHandler {
             }
         }
     }
+    private void showTrainers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lista de Treinadores");
+        TrainerHandler trainerHandler = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        trainerHandler.showAllTrainers();
 
-    private void deleteAthlete() {
+        System.out.println("");
+        System.out.println(" 1- Adicionar novo treinador");
+        System.out.println(" 2- Voltar ");
+        System.out.println(" 3- Sair ");
+
+        while (true) {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addTrainerMenu();
+                    showTrainersMenu();
+                    return;
+                case 2:
+                    showTrainersMenu();
+                    return;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Opção incorreta: " + choice);
+                    System.out.println("Por favor, escolha uma opção válida.");
+            }
+        }
+    }
+    private void addTrainerMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("----Adicionar Treinador----");
+        System.out.println("Introduza o nome do treinador");
+        trainerName = scanner.nextLine();
+
+        System.out.println("Pretendo associar o treinador a uma equipa?");
+        System.out.println("1- Sim");
+        System.out.println("2- Não");
+
+        while (true) {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addTrainerToTeam();
+                    return;
+                case 2:
+                    addTrainerNoTeam();
+                    return;
+                default:
+                    System.out.println("Opção incorreta: " + choice);
+                    System.out.println("Por favor, escolha uma opção válida.");
+            }
+        }
+
+    }
+
+    private void addTrainerNoTeam() {
+        TrainerHandler trainerHandler = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        trainerHandler.insertTrainer();
+    }
+
+    private void addTrainerToTeam() {
+        TrainerHandler trainerHandler = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        trainerHandler.insertTrainer();
+
+        TrainerHandler getTrainerId = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        int trainerId = getTrainerId.getLastTrainerInserted();
+
+        showTeamOptions();
+        showGenderOptions();
+
+        TrainerHandler getTeamId = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        int teamId = getTeamId.getTeamId();
+
+        TrainerHandler associateTrainer = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        associateTrainer.addTrainerToTeam(trainerId, teamId);
+
+        System.out.println("Treinador associado a uma equipa com sucesso!");
+
     }
 
     private void addAthleteToTeam() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isValid = false;
+        boolean alreadyExist = false;
+        int trainerId;
+        System.out.println("Lista de treinadores");
+        System.out.println("");
+        TrainerHandler trainerHandler = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        trainerHandler.showTrainersList();
+        System.out.println("");
+
+        do {
+            System.out.println("Insira o ID do treinador que quer associar a uma equipa");
+            trainerId = scanner.nextInt();
+            isValid = trainerHandler.isValidId(trainerId);
+        } while (!isValid);
+
+        showTeamOptions();
+        showGenderOptions();
+
+        TrainerHandler trainerTeamId = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        int teamID = trainerTeamId.getTeamId();
+
+        alreadyExist = trainerHandler.seeAlreadyAssociate(trainerId, teamID);
+
+        if (alreadyExist == false) {
+            trainerHandler.insertIntoTeam(trainerId, teamID);
+        } else {
+            System.out.println("Este treinador já está associado a esta equipa");
+        }
     }
 
-    private void addTrainer(boolean b) {
+    private void deleteAthlete() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isValid;
+        int trainerId;
+        TrainerHandler trainerHandler = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        trainerHandler.showAllTrainers();
+
+        do {
+            System.out.println("Insira o ID do treinador que quer apagar");
+            trainerId = scanner.nextInt();
+            isValid = trainerHandler.isValidId(trainerId);
+        } while (!isValid);
+
+        TrainerHandler getTrainerById = new TrainerHandler(trainerName, trainerTeamName, trainerTeamGender);
+        getTrainerById.showTrainerById(trainerId);
+
+        //AthleteHandler athleteHandlerTeam = new AthleteHandler(athleteName, athleteNumber, athleteTeam, athleteGender);
     }
 
-    private void showTrainersFilterTeam() {
-    }
 }
